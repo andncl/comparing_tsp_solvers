62060a6f5bb14d34061ced155798047f5465dd23
Andreas Nickl <59257257+andncl@users.noreply.github.com>
2023-10-27 15:23:35 +1100

    Added brute force solver class

---
+++ src/brute_force_solver.py
@@ -0,0 +1,29 @@
+"""Module containing SimulatedAnnealer class"""
+import copy
+import logging
+
+import numpy as np
+import matplotlib.pyplot as plt
+from ipywidgets import interact
+import ipywidgets as widgets
+
+from .tsp_solver_base import TspSolverBase
+
+class BruteForceSolver(TspSolverBase):
+    """ 
+    The SimulatedAnnealer class formulates and solves the traveling salesman
+    problem with the classical simulated annealing algorithm
+    """
+    def __init__(self) -> None:
+        super().__init__()
+        self.current_order = None
+        self.current_distance = None
+
+    def solve_tsp(self, coordinates: np.ndarray = None) -> dict:
+        """
+        Calculates all possible combinations of paths and finds the minimum
+        
+        Args:
+            coordinates (numpy.ndarray): Coordinates of the given cities
+        """
+        self.current_distance()
