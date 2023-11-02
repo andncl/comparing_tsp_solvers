f3fdec1da1e9433cba384ee0ffd6cd9f49cdcedc
andi <andreas@orangeqs.com>
2023-11-02 23:45:37 +1100

    Updated fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
 import pytest
 from src.tsp_solver_base import TspSolverBase
 
+class DummySolver(TspSolverBase):
+    def solve_tsp(self, coordinates):        
+        return None
+
 @pytest.fixture
 def dummy_solver():
-    yield TspSolverBase()
+    yield DummySolver()
